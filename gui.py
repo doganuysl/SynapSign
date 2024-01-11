0e23ef708f08ddaf87aa84496ddc62105f03bcf1
doganuysl <erdoganuysalbs@gmail.com>
2024-01-11 17:10:42 +0300

    Advanced Topics in Human Computer Interaction and Usability first commit

---
+++ gui.py
@@ -0,0 +1,113 @@
+import pickle
+import cv2
+import mediapipe as mp
+import numpy as np
+import PySimpleGUI as sg
+
+# Kamera erişimi için izin iste
+layout_permission = [[sg.Text('Bu uygulama kameranızı kullanacak. İzin vermek istiyor musunuz?')],
+                     [sg.Button('İzin Ver'), sg.Button('İzin Verme')]]
+
+window_permission = sg.Window('Kamera İzni', layout_permission)
+
+event, values = window_permission.read()
+
+if event == 'İzin Ver':
+    window_permission.close()
+
+    # Kamera için ana arayüz
+    layout = [[sg.Text('SynapSign', size=(20, 1), font=('Any', 18), text_color='black', justification='center')],
+              [sg.Image(filename='snap.png', key='image')],
+              [sg.Button('Exit', size=(10, 1), pad=((200, 0), 3), font='Helvetica 14')]]
+
+    window = sg.Window('SynapSign', layout, finalize=True, element_justification='center', location=(0, 0))
+
+    # Modelin yüklenmesi
+    model_dict = pickle.load(open('./model.p', 'rb'))
+    model = model_dict['model']
+
+    cap = cv2.VideoCapture(0)
+
+    mp_hands = mp.solutions.hands
+    mp_drawing = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
+
+    hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.5, max_num_hands=1)
+
+    labels_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L',
+                   12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W',
+                   23: 'X', 24: 'Y', 25: 'Z', 26:'What a Shame!'}
+
+    while True:
+        event, values = window.read(timeout=20)
+
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            break
+
+        ret, frame = cap.read()
+
+        H, W, _ = frame.shape
+
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        results = hands.process(frame_rgb)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    frame,  # image to draw
+                    hand_landmarks,  # model output
+                    mp_hands.HAND_CONNECTIONS,  # hand connections
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style())
+
+            data_aux = []
+            x_ = []
+            y_ = []
+
+            for hand_landmarks in results.multi_hand_landmarks:
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+
+                    x_.append(x)
+                    y_.append(y)
+
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    data_aux.append(x - min(x_))
+                    data_aux.append(y - min(y_))
+
+                min_x = min(x_)
+                min_y = min(y_)
+                max_x = max(x_)
+                max_y = max(y_)
+
+                x1 = int(min_x * W) - 10
+                y1 = int(min_y * H) - 10
+                x2 = int(max_x * W) - 10
+                y2 = int(max_y * H) - 10
+
+                # Modelin beklendiği özellik sayısına göre data_aux'u düzenle
+                # Örnek olarak, eğer model 84 özellik bekliyorsa ve data_aux'ta 42 özellik varsa:
+                data_aux.extend([0] * (84 - len(data_aux)))  # Eksik özellikleri sıfırlarla tamamla veya uygun bir yöntemle doldur
+
+                prediction = model.predict([np.asarray(data_aux)])
+
+                predicted_character = labels_dict[int(prediction[0])]
+
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 2)
+                cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
+                            cv2.LINE_AA)
+
+        imgbytes = cv2.imencode('.png', frame)[1].tobytes()
+        window['image'].update(data=imgbytes)
+
+    window.close()
+    cap.release()
+    cv2.destroyAllWindows()
+
+else:
+    sg.popup('Uygulama kapatıldı!', title='Kamera İzni', keep_on_top=True)
+
+window_permission.close()
---
+++ train_classifier.py
@@ -0,0 +1,37 @@
+import pickle
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+
+data_dict = pickle.load(open('./data.pickle', 'rb'))
+
+# Veri setinizdeki farklı uzunluktaki listeleri aynı boyutta listelere dönüştürme
+max_length = max(len(item) for item in data_dict['data'])  # En uzun liste uzunluğunu al
+data_processed = []
+
+for item in data_dict['data']:
+    # Eksik veriyi tamamlayarak, tüm listeleri aynı boyuta getirme
+    item += [0] * (max_length - len(item))  # Örnek olarak eksik veriyi sıfırlarla tamamladık
+    data_processed.append(item)
+
+data = np.asarray(data_dict['data'])
+labels = np.asarray(data_dict['labels'])
+
+x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
+
+model = RandomForestClassifier()
+
+model.fit(x_train, y_train)
+
+y_predict = model.predict(x_test)
+
+score = accuracy_score(y_predict, y_test)
+
+print('{}% of samples were classified correctly !'.format(score * 100))
+
+f = open('model.p', 'wb')
+pickle.dump({'model': model}, f)
+f.close()
